6fd67615b441a62e205a83946142c9ef43b36ff1
Jhabriel Varela <jhabriel@gmail.com>
2022-09-12 00:32:37 +0200

    TST: Create a first dummy test

---
+++ tests/functional/test_dummy.py
@@ -0,0 +1,11 @@
+import porepy as pp
+import pytest
+import numpy as np
+
+
+class TestDummy:
+    """Create some dummy test"""
+
+    def test_check_simple_sum(self):
+        """Tests a simple sum"""
+        assert 1 + 1 == 2
