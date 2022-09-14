4cbac32e7dd3c6ea148726a2fb7fa5fcc1f3f67a
Jhabriel Varela <jhabriel@gmail.com>
2022-09-14 14:15:45 +0200

    Update test_dummy.py

--- tests/functional/test_dummy.py
+++ tests/functional/test_dummy.py
@@ -9,3 +9,7 @@ class TestDummy:
     def test_check_simple_sum(self):
         """Tests a simple sum"""
         assert 1 + 1 == 2
+
+    def test_check_fail_test(self):
+        """Make a failing test on purpose"""
+        assert 1 == 2
