391a2896f743a54d4b43f75a788c32658691d0d3
Jhabriel Varela <jhabriel@gmail.com>
2022-09-14 21:04:53 +0200

    Update test_dummy.py

--- tests/functional/test_dummy.py
+++ tests/functional/test_dummy.py
@@ -2,14 +2,14 @@ import porepy as pp
 import pytest
 import numpy as np
 
-
 class TestDummy:
     """Create some dummy test"""
 
-    def test_check_simple_sum(self):
-        """Tests a simple sum"""
+    def test_one(self):
         assert 1 + 1 == 2
 
-    def test_check_fail_test(self):
-        """Make a failing test on purpose"""
-        assert 1 == 7
+    def test_two(self):
+        assert 1 == 1
+
+    def test_three(self):
+        assert 5 + 5 == 12 - 2
